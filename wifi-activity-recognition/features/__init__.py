c397f29a91e225a9828c287cc6e485ba7fd8f895
Diogo Ribeiro <diogo_dj@hotmail.com>
2025-08-19 16:09:44 +0100

    test: add coverage for new feature extractors

--- wifi-activity-recognition/features/__init__.py
+++ wifi-activity-recognition/features/__init__.py
@@ -4,9 +4,23 @@ from .advanced_features import sample_entropy, spectral_entropy, statistical_mom
 from .correlation import correlation_matrix
 from .cv_transforms import magnitude_to_uint8
 from .doppler import doppler_spectrum
+from .fractal_features import higuchi_fd, katz_fd
 from .frequency_domain import compute_fft, power_spectrum
+from .graph_features import (
+    build_correlation_graph,
+    centrality_measures,
+    community_structure,
+    spectral_graph_features,
+)
+from .information_theory import mutual_information, shannon_entropy
 from .spectrogram import compute_spectrogram
 from .time_domain import compute_rms, zero_crossing_rate
+from .wavelet_features import (
+    cwt_coefficients,
+    dwt_energy,
+    scale_energy,
+    wavelet_packet_energy,
+)
 
 __all__ = [
     "compute_rms",
@@ -20,4 +34,16 @@ __all__ = [
     "statistical_moments",
     "spectral_entropy",
     "sample_entropy",
+    "cwt_coefficients",
+    "dwt_energy",
+    "wavelet_packet_energy",
+    "scale_energy",
+    "higuchi_fd",
+    "katz_fd",
+    "build_correlation_graph",
+    "centrality_measures",
+    "community_structure",
+    "spectral_graph_features",
+    "shannon_entropy",
+    "mutual_information",
 ]
