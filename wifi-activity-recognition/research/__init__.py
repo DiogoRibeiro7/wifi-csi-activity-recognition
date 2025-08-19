5a92963b3917f134b2b383d56565abbc4b782778
Diogo Ribeiro <diogo_dj@hotmail.com>
2025-08-19 16:09:58 +0100

    feat: expand few-shot meta-learning

--- wifi-activity-recognition/research/__init__.py
+++ wifi-activity-recognition/research/__init__.py
@@ -1,6 +1,17 @@
 """Research-oriented extensions for WiFi activity recognition."""
 
 from .domain_adaptation import DomainAdapter
-from .few_shot_learning import FewShotLearner
+from .few_shot_learning import (
+    FewShotLearner,
+    MAMLLearner,
+    PrototypicalNetwork,
+    RelationNetwork,
+)
 
-__all__ = ["DomainAdapter", "FewShotLearner"]
+__all__ = [
+    "DomainAdapter",
+    "FewShotLearner",
+    "MAMLLearner",
+    "PrototypicalNetwork",
+    "RelationNetwork",
+]
